cef6be80ce0afa1ce5a03f3296600c4e7ab46cd0
Stephen Pendino <Stephen Pendino>
2016-12-18 21:04:34 -0500

    Chapter 4

---
+++ Chapter4/exercises/03 - FizzBuzz/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+			// fmt.Println(i, "FizzBuzz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+			// fmt.Println(i, "Buzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+			// fmt.Println(i, "Fizz")
+		}
+	}
+}
