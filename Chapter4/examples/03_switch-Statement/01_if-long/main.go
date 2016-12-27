8abae0bb751fac111dbbe92f617ba26bb071e400
Stephen Pendino <Stephen Pendino>
2016-12-27 12:11:44 -0500

    finish Chapter4 and start chapter 5

---
+++ Chapter4/examples/03_switch-Statement/01_if-long/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i <= 5; i++ {
+		if i == 0 {
+			fmt.Println("zero")
+		} else if i == 1 {
+			fmt.Println("one")
+		} else if i == 2 {
+			fmt.Println("two")
+		} else if i == 3 {
+			fmt.Println("three")
+		} else if i == 4 {
+			fmt.Println("four")
+		} else if i == 5 {
+			fmt.Println("five")
+		}
+
+	}
+}
