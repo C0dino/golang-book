4c13fb13f858829a53cb57c2fa4868c2e9ccd562
c0dino <spendino@gmail.com>
2017-02-15 21:01:27 -0500

    main

---
+++ Chapter10-Concurrency/channels/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
