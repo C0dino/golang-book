6cacc432ce03e086d5cc13085a2e9d05a9451258
c0dino <spendino@gmail.com>
2017-02-22 19:18:51 -0500

    structs

--- Chapter7-Structs&Interfaces/02_Structs/main.go
+++ Chapter7-Structs&Interfaces/02_Structs/main.go
@@ -5,6 +5,14 @@ import (
 	"math"
 )
 
+type circle struct {
+	x, y, r float64
+	// or
+	// x float64
+	// y float64
+	// r float64
+}
+
 func distance(x1, y1, x2, y2 float64) float64 {
 	a := x2 - x1
 	b := y2 - y1
@@ -12,19 +20,28 @@ func distance(x1, y1, x2, y2 float64) float64 {
 }
 
 func rectangleArea(x1, y1, x2, y2 float64) float64 {
-	l := distance(x1, y1, x2, y2)
-	w := distance(x1, y1, x2, y2)
+	l := distance(x1, y1, x1, y2)
+	w := distance(x1, y1, x2, y1)
 	return l * w
 }
 
 func circleArea(x, y, r float64) float64 {
 	return math.Pi * r * r
 }
+
 func main() {
+
+	c := circle{0, 0, 5}
+	//or
+	//	c := circle{x: 0, y: 0, r: 5}
+
+	fmt.Println(circleArea(c))
+
 	var rx1, ry1 float64 = 0, 0
 	var rx2, ry2 float64 = 10, 10
 	var cx, cy, cr float64 = 0, 0, 5
 
 	fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
 	fmt.Println(circleArea(cx, cy, cr))
+
 }
